76b3560ea2802cf730ee05fa095c3018c2eb2f47
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2024-09-28 12:08:11 +0530

    Update shop.jsx

--- shopping-cart-react-main/src/pages/shop/shop.jsx
+++ shopping-cart-react-main/src/pages/shop/shop.jsx
@@ -9,6 +9,7 @@ export const Shop = () => {
       <div className="shopTitle">
         <h1 ShiwanshuTech Shop</h1>
       </div>
+    
       <div className="products">
         {PRODUCTS.map((product) => (
           <Product data={product} />
